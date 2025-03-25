7e9a4f5e0701315c8578a49714287b544e384e29
lucas.kocheida@ynov.com <lucas.kocheida@ynov.com>
2025-03-25 14:25:10 +0100

    Add Expense class to represent an expense with a description and amount

--- src/model/Expense.java
+++ src/model/Expense.java
@@ -1,37 +1,11 @@
 package model;
 
-import java.util.Date;
-
 public class Expense {
-    private int id;
-    private double amount;
     private String description;
-    private Date date;
-    private String category;
+    private double amount;
 
-    // Constructor
-    public Expense(int id, double amount, String description, Date date, String category) {
-        this.id = id;
-        this.amount = amount;
+    public Expense(String description, double amount) {
         this.description = description;
-        this.date = date;
-        this.category = category;
-    }
-
-    // Getters and Setters
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public double getAmount() {
-        return amount;
-    }
-
-    public void setAmount(double amount) {
         this.amount = amount;
     }
 
@@ -43,31 +17,16 @@ public class Expense {
         this.description = description;
     }
 
-    public Date getDate() {
-        return date;
+    public double getAmount() {
+        return amount;
     }
 
-    public void setDate(Date date) {
-        this.date = date;
+    public void setAmount(double amount) {
+        this.amount = amount;
     }
 
-    public String getCategory() {
-        return category;
-    }
-
-    public void setCategory(String category) {
-        this.category = category;
-    }
-
-    // toString method for easy printing
     @Override
     public String toString() {
-        return "Expense{" +
-                "id=" + id +
-                ", amount=" + amount +
-                ", description='" + description + '\'' +
-                ", date=" + date +
-                ", category='" + category + '\'' +
-                '}';
+        return description + ": €" + amount;
     }
 }
