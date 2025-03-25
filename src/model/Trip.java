5a27363cf4aa99bc6f2e4562b68b6fcb0492a1e2
lucas.kocheida@ynov.com <lucas.kocheida@ynov.com>
2025-03-25 14:23:21 +0100

    Add Trip class to represent a trip with a destination

--- src/model/Trip.java
+++ src/model/Trip.java
@@ -1,31 +1,10 @@
 package model;
 
-import java.util.Date;
-import java.util.List;
-
 public class Trip {
-    private int id;
     private String destination;
-    private Date startDate;
-    private Date endDate;
-    private List<Expense> expenses;
 
-    // Constructor
-    public Trip(int id, String destination, Date startDate, Date endDate, List<Expense> expenses) {
-        this.id = id;
+    public Trip(String destination) {
         this.destination = destination;
-        this.startDate = startDate;
-        this.endDate = endDate;
-        this.expenses = expenses;
-    }
-
-    // Getters and Setters
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
     }
 
     public String getDestination() {
@@ -36,39 +15,8 @@ public class Trip {
         this.destination = destination;
     }
 
-    public Date getStartDate() {
-        return startDate;
-    }
-
-    public void setStartDate(Date startDate) {
-        this.startDate = startDate;
-    }
-
-    public Date getEndDate() {
-        return endDate;
-    }
-
-    public void setEndDate(Date endDate) {
-        this.endDate = endDate;
-    }
-
-    public List<Expense> getExpenses() {
-        return expenses;
-    }
-
-    public void setExpenses(List<Expense> expenses) {
-        this.expenses = expenses;
-    }
-
-    // toString method for easy printing
     @Override
     public String toString() {
-        return "Trip{" +
-                "id=" + id +
-                ", destination='" + destination + '\'' +
-                ", startDate=" + startDate +
-                ", endDate=" + endDate +
-                ", expenses=" + expenses +
-                '}';
+        return destination;
     }
 }
