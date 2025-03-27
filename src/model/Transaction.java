fcf0dfde38f8efd9b2d117d31c05acf2056fddb4
lucas.kocheida@ynov.com <lucas.kocheida@ynov.com>
2025-03-27 13:53:57 +0100

    Add Transaction class : -This class represents a financial transaction with a description and an amount.

---
+++ src/model/Transaction.java
@@ -0,0 +1,23 @@
+package model;
+
+public class Transaction {
+    private String description;
+    private double amount;
+
+    public Transaction(String description, double amount) {
+        this.description = description;
+        this.amount = amount;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+    @Override
+    public String toString() {
+        return description + ": €" + amount;
+    }
+}
