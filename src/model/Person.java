bdc025909987ba617bae3a19fa6782698eaaadb8
lucas.kocheida@ynov.com <lucas.kocheida@ynov.com>
2025-03-27 13:52:00 +0100

    Add Person class : -represents a person with a name and an email.

---
+++ src/model/Person.java
@@ -0,0 +1,18 @@
+package model;
+
+public class Person {
+    private String name;
+    private String email;
+
+    public Person(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public String getEmail() {
+        return email;
+    }
+}
